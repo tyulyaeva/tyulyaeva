2bcb74768c7f20471a7cdda0cce422ace86f8569
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-04-16 20:14:34 +0300

    second

--- src/test/java/SearchSelenide.java
+++ src/test/java/SearchSelenide.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchSelenide {
@@ -21,6 +20,30 @@ public class SearchSelenide {
         $("#wiki-tab").shouldBe(visible).click();
         $("#wiki-body a[href$='SoftAssertions']").shouldBe(visible).click();
         $(".page").shouldHave(text("Using JUnit5 extend test class:"));
-
+        $("#wiki-body").$(".markdown-body").shouldHave(
+                text("class Tests {\n" +
+                        "  @RegisterExtension \n" +
+                        "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
+                        "\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"),
+                text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"));
     }
 }
