0228aee62fc9780f0d27e4c97828b2a31aa6e3ba
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-04-17 20:46:24 +0300

    HoverTest

---
+++ src/test/java/HoverTest.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class HoverTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void FifthLesson() {
+        open(baseUrl);
+        $(".HeaderMenu-nav").shouldHave(text("Solutions")).$(byText("Solutions")).hover();;
+        $(byTagAndText("a","Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(
+                text("The AI-powered\n" +
+                        "developer platform"));
+    }
+}
