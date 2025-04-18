f0d69613047519265a1614879af270b3bbca20f3
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-04-18 15:01:14 +0300

    GoogleSerch

---
+++ src/test/java/FirstLesson.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.FileDownloadMode;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FirstLesson {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        baseUrl = "https://the-internet.herokuapp.com/drag_and_drop";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.fileDownload = FileDownloadMode.FOLDER;
+    }
+
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+
+    }
+}
