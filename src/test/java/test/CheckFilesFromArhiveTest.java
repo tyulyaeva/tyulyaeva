d40db2a88a8e4e7a9b43326e7e945af50823e945
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-05-21 18:09:51 +0300

    HW9_CheckFilesFromArhiveTest_CheckJsonFileTest_fixed

--- src/test/java/test/CheckFilesFromArhiveTest.java
+++ src/test/java/test/CheckFilesFromArhiveTest.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import static org.assertj.core.api.Assertions.*;
 
 public class CheckFilesFromArhiveTest {
 
@@ -20,23 +22,30 @@ public class CheckFilesFromArhiveTest {
     @Test
     @DisplayName("Проверка наличия файлов в zip архиве")
     void checkArhiveFileTest() throws Exception {
-
         try (ZipInputStream arhive = new ZipInputStream(classLoaderArhiveFile.getResourceAsStream("studentPersonData.zip"))) {
+            List<String> fileNames = new ArrayList<>();
             ZipEntry entry;
             while ((entry = arhive.getNextEntry()) != null) {
+                fileNames.add(entry.getName());
                 System.out.println(entry.getName());
             }
+            assertThat(fileNames)
+                    .as("Проверка что архив содержит файлы")
+                    .isNotEmpty()
+                    .hasSizeGreaterThan(0);
         }
     }
 
     @Test
     @DisplayName("Проверка значений в .csv файле из архива")
     void csvCheckArhiveTest() throws Exception {
+        boolean csvFound = false;
         try (InputStream inputStream = classLoaderArhiveFile.getResourceAsStream("studentPersonData.zip");
-             ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
+            ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
             ZipEntry zipEntry;
             while ((zipEntry = zipInputStream.getNextEntry()) != null) {
                 if (zipEntry.getName().endsWith(".csv")) {
+                    csvFound = true;
                     CSVReader csvReader = new CSVReader(new InputStreamReader(zipInputStream));
                     List<String[]> content = csvReader.readAll();
                     org.assertj.core.api.Assertions.assertThat(content).contains(
@@ -44,17 +53,22 @@ public class CheckFilesFromArhiveTest {
                     );
                 }
             }
+            assertThat(csvFound)
+                    .as("Проверка наличия в архиве .csv файла")
+                    .isTrue();
         }
     }
 
     @Test
     @DisplayName("Проверка значений в .xslx файле из архива")
     void xlsxCheckArhiveTest() throws Exception {
+        boolean xlsFound = false;
         try (InputStream inputStream = classLoaderArhiveFile.getResourceAsStream("studentPersonData.zip");
              ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
             ZipEntry zipEntry;
             while ((zipEntry = zipInputStream.getNextEntry()) != null) {
                 if (zipEntry.getName().endsWith(".xlsx")) {
+                    xlsFound = true;
                     XLS xls = new XLS(zipInputStream);
                     String actualResult1 = xls.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue();
                     String actualResult2 = xls.excel.getSheetAt(0).getRow(1).getCell(0).getStringCellValue();
@@ -62,6 +76,9 @@ public class CheckFilesFromArhiveTest {
                     Assertions.assertEquals("Inna Tyulyaeva", actualResult2);
                 }
             }
+            assertThat(xlsFound)
+                    .as("Проверка наличия в архиве .xls файла")
+                    .isTrue();
         }
     }
 
@@ -69,16 +86,20 @@ public class CheckFilesFromArhiveTest {
     @Test
     @DisplayName("Проверка значений в .pdf файле из архива")
     void pdfCheckArhiveTest() throws Exception {
+        boolean pdfFound = false;
         try (InputStream inputStream = classLoaderArhiveFile.getResourceAsStream("studentPersonData.zip");
              ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
             ZipEntry zipEntry;
             while ((zipEntry = zipInputStream.getNextEntry()) != null) {
                 if (zipEntry.getName().endsWith(".pdf")) {
+                    pdfFound = true;
                     PDF pdf = new PDF(zipInputStream);
                     Assertions.assertTrue(pdf.text.contains("We confirm that on March 19, 2024 at 15:06 Moscow time"));
                 }
             }
-
+            assertThat(pdfFound)
+                    .as("Проверка наличия в архиве найден .pdf файла")
+                    .isTrue();
         }
     }
 }
