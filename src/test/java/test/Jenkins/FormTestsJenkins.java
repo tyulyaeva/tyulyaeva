da82d16cd8c20a49edf9950ea936dfcd39924197
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-05-21 14:00:21 +0300

    Allure_Jenkins_test

---
+++ src/test/java/test/Jenkins/FormTestsJenkins.java
@@ -0,0 +1,55 @@
+package test.Jenkins;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+@DisplayName("Allure/Jenkins тест")
+
+public class FormTestsJenkins {
+
+    @BeforeAll
+    static void setupConfig(){
+        Configuration.browserSize = "1920x1080";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.pageLoadStrategy ="eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+    }
+
+    @Tag("automation-practice-form")
+    @Feature("Форма регистрации 'automation-practice-form")
+    @Story("Тестирование формы регистрации 'automation-practice-form")
+    @Owner("inna_tyulyaeva")
+    @DisplayName("Успешная отправка формы регистрации со всеми заполненными полями")
+    @Test
+    void fillFormTest() {
+        WebGithubStepsJenkins steps = new WebGithubStepsJenkins();
+           steps.openMainPage()
+                .setValueFormTest ()
+                .checkValueFormTest ();
+    }
+}
