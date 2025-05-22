c367a1f2bff61a79908c816628ee07f8535b339a
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-05-22 12:35:21 +0300

    HW12_Allure_Jenkins_Param_6

--- src/test/java/test/Jenkins/FakerDataTestJenkins.java
+++ src/test/java/test/Jenkins/FakerDataTestJenkins.java
@@ -15,35 +15,35 @@ import java.util.Map;
 @DisplayName("Allure/Jenkins FakerData тест")
 
 public class FakerDataTestJenkins extends TestBase {
-    private static final String SELENOID_URL = System.getProperty("selenoid.url");
-    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");
-    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
+//    private static final String SELENOID_URL = System.getProperty("selenoid.url");
+//    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");
+//    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
 
-    @BeforeAll
-    static void setupConfig(){
-        Configuration.browserSize = "1920x1080";
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.pageLoadStrategy ="eager";
-        Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//    @BeforeAll
+//    static void setupConfig(){
+//        Configuration.browserSize = "1920x1080";
+//        SelenideLogger.addListener("allure", new AllureSelenide());
+//        Configuration.baseUrl = "https://demoqa.com/";
+//        Configuration.pageLoadStrategy ="eager";
+//        Configuration.timeout = 10000;
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//        Configuration.browserCapabilities = capabilities;
+//    }
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attachments.screenshotAs("Last screenshot");
-        Attachments.pageSource();
-        Attachments.browserConsoleLogs();
-        Attachments.addVideo();
-        Selenide.closeWebDriver();
-    }
+//    @AfterEach
+//    void addAttachments() {
+//        Attachments.screenshotAs("Last screenshot");
+//        Attachments.pageSource();
+//        Attachments.browserConsoleLogs();
+//        Attachments.addVideo();
+//        Selenide.closeWebDriver();
+//    }
 
     @Tag("demoqa")
     @Feature("Форма регистрации 'automation-practice-form")
--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,12 +1,52 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import test.Jenkins.Attachments;
+
+import java.util.Map;
 
 public class TestBase {
+
+    static String selenoidUserLogin = System.getProperty("selenoidUserLogin", "user1");
+    static String selenoidUserPassword = System.getProperty("selenoidUserPassword", "1234");
+    static String selenoidRemoteServerUrl = System.getProperty(
+            "selenoidRemoteServerUrl", "selenoid.autotests.cloud");
+    static String browser = System.getProperty("browser", "chrome");
+    static String browserVersion = System.getProperty("browserVersion", "128.0");
+    static String browserSize = System.getProperty("browserResolution", "1920x1080");
+
     @BeforeAll
     static void setupConfig () {
-        Configuration.browserSize = "1920x1080";
+        //Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+
+        Configuration.browserSize = browserSize;
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+        Configuration.remote = "https://" +
+                selenoidUserLogin + ":" + selenoidUserPassword +"@" + selenoidRemoteServerUrl + "/wd/hub";
+        Configuration.remote = "https://" +
+                selenoidUserLogin + ":" + selenoidUserPassword +"@" + selenoidRemoteServerUrl + "/wd/hub";
+
+        // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+        Selenide.closeWebDriver();
     }
 }
