7ff77eb99376b1e865936e882d04efc57ac770df
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-05-22 13:08:56 +0300

    HW12_Allure_Jenkins_Param_8

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -6,31 +6,47 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import test.Jenkins.Attachments;
 
 import java.util.Map;
+import java.util.UUID;
 
 public class TestBase {
     private static final String SELENOID_URL = System.getProperty("selenoid.url");
     private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");
     private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
 
-    @BeforeAll
-    static void setupConfig(){
-        Configuration.browserSize = "1920x1080";
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.pageLoadStrategy ="eager";
-        Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//    @BeforeAll
+//    static void setupConfig(){
+//        Configuration.browserSize = "1920x1080";
+//        SelenideLogger.addListener("allure", new AllureSelenide());
+//        Configuration.baseUrl = "https://demoqa.com/";
+//        Configuration.pageLoadStrategy ="eager";
+//        Configuration.timeout = 10000;
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//        Configuration.browserCapabilities = capabilities;
+//    }
 
+    @BeforeEach
+    public void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
-                "enableVideo", true
+                "enableVideo", true,
+                "name", "Test: " + UUID.randomUUID()
         ));
+        Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
         Configuration.browserCapabilities = capabilities;
+        Configuration.holdBrowserOpen = false;
     }
 
     @AfterEach
