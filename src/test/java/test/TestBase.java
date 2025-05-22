bbe4af0fbe4f410acc9a4d9babf41a43356eb844
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-05-22 13:26:40 +0300

    HW12_Allure_Jenkins_Param_8

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,5 +1,7 @@
 package test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -14,9 +16,9 @@ import java.util.Map;
 import java.util.UUID;
 
 public class TestBase {
-    private static final String SELENOID_URL = System.getProperty("selenoid.url");
-    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");
-    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
+//    private static final String SELENOID_URL = System.getProperty("selenoid.url");
+//    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");
+//    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
 
     @BeforeAll
     static void setupConfig(){
@@ -33,26 +35,53 @@ public class TestBase {
 //                "enableVideo", true
 //        ));
 //        Configuration.browserCapabilities = capabilities;
-    Configuration.browser = System.getProperty("browser", "chrome");
-    Configuration.browserVersion = System.getProperty("browser.version", "128.0");
-    Configuration.browserSize = System.getProperty("browser.size", "1920x1080");
-    Configuration.baseUrl = "https://demoqa.com/";
-    Configuration.pageLoadStrategy = "eager";
-    }
+        String selenoidHost = System.getProperty("selenoid_host", "selenoid.autotests.cloud");
+        String selenoidLogin = System.getProperty("selenoid_login", "user1");
+        String selenoidPassword = System.getProperty("selenoid_password", "1234");
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browserVersion", "127.0");
+        String screenResolution = System.getProperty("screenResolution", "1920x1080");
 
-    @BeforeEach
-    public void beforeEach() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebDriverManager.chromedriver()
+                .clearDriverCache()
+                .clearResolutionCache()
+                .setup();
+
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.browserSize = screenResolution;
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
+                selenoidLogin,
+                selenoidPassword,
+                selenoidHost);
+
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
-                "enableVideo", true,
-                "name", "Test: " + UUID.randomUUID()
+                "enableVideo", true
         ));
-        Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
         Configuration.browserCapabilities = capabilities;
-        Configuration.holdBrowserOpen = false;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
+
+//    @BeforeEach
+//    public void beforeEach() {
+//        SelenideLogger.addListener("allure", new AllureSelenide());
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true,
+//                "name", "Test: " + UUID.randomUUID()
+//        ));
+//        Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
+//        Configuration.browserCapabilities = capabilities;
+//        Configuration.holdBrowserOpen = false;
+//    }
 
     @AfterEach
     void addAttachments() {
